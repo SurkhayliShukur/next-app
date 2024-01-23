13d453fe7f753aad59b6f2ec34936aaf5fe9d219
Shukur939 <shukursurkhayli000@gmail.com>
2024-01-23 15:31:48 +0400

    reactQuery

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {QueryClient,QueryClientProvider} from "react-query"
+import {ToastContainer} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient()
+ return(
+    <QueryClientProvider client = {queryClient}>
+      <main>
+        <Component {...pageProps}/>
+        <ToastContainer/>
+      </main>
+    </QueryClientProvider>
+ )
 }
