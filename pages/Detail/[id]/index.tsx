bab97572a0a46a29d5fa259a444b1112697d21f4
Shukur939 <shukursurkhayli000@gmail.com>
2024-01-27 10:12:46 +0400

    post

--- pages/Detail/[id]/index.tsx
+++ pages/Detail/[id]/index.tsx
@@ -1,13 +1,41 @@
 import React from 'react'
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next"
 import { useRouter } from "next/router"
+import { getSinglePost } from "../../../config/posts"
+import  Head  from 'next/head'
+import {ROUTER} from "../../../shared/constant/router"
 
 
-const Detail: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = () => {
+const Detail: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = ({post}) => {
     const { push, query } = useRouter()
-    
     return (
         <>
+        <Head>
+            Detail Page
+        </Head>
+        <div className='card lg:card-side bg-base-100 shadow-xl'>
+        <figure className="w-4/6">
+          <img
+            src={post?.image}
+            alt={post?.title}
+            className="h-screen  object-cover"
+          />
+        </figure>
+        <div className="card-body p-6">
+          <h2 className="card-title text-3xl text-sky-300">
+            {post?.title.slice(0, 16)}
+          </h2>
+          <p className="text-xl text-sky-300 my-4">{post?.body}</p>
+          <div className="card-actions justify-end">
+            <button
+              className="btn btn-secondary text-gray-200 px-10 text-3xl"
+              onClick={() => push(`${query.id}/${ROUTER.Action}`)}
+            >
+              Actions
+            </button>
+          </div>
+        </div>
+        </div>
         </>
     )
 }
@@ -15,5 +43,24 @@ const Detail: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> =
 export default Detail;
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const postId = context.query.id as string | number
+    try {
+        const response = await getSinglePost(postId)
+        const post = response.data
 
+        return{
+            props: {
+                post,
+            }
+        }
+    }
+    catch (error) {
+        console.error("error fetching data", error)
+        return {
+            props: {
+              post: null,
+              isError: true,
+            },
+          };
+    }
 }
