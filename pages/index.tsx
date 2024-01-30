9f03a0c05b0f9f6070a84b3e98ed66ce2b39c31d
Shukur939 <shukursurkhayli000@gmail.com>
2024-01-30 16:32:51 +0400

    last

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,24 @@ import { GetServerSideProps, InferGetServerSidePropsType } from "next"
 import Head from "next/head"
 import Card from "@/shared/components/Card"
 import { getPost } from "@/config/posts"
-import {PostDataType} from "../interface/data"
+import { PostDataType } from "../interface/data"
 
-const Home: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ data }) => {
+const Home: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ posts }) => {
   return (
     <>
       <Layout>
         <Head>
           <title>Blog Page</title>
         </Head>
-        {data && data.length>0 ? (
+        {posts && posts.length > 0 ? (
           <ul className="p-14 grid gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
             {
-              data.map((post:PostDataType) =>(
-                <Card {...post} key={post.id}/>
-              ) )
+              posts.map((post: PostDataType) => (
+                <Card {...post} key={post.id} />
+              ))
             }
           </ul>
-        ):(
+        ) : (
           <p className="text-center text-5xl pt-60 text-gray-500 capitalize font-extrabold">NO DATA</p>
         )}
       </Layout>
@@ -33,11 +33,12 @@ const Home: React.FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (
 export const getServerSideProps: GetServerSideProps = async () => {
   try {
     const response = await getPost()
-    const data = response.data
+    const posts = response.data.filter((post) => (post.id as number) > 100)
+    // const data = response.data
 
     return {
       props: {
-        data,
+        posts,
       }
     }
   }
